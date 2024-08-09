1b685abc73263c6f1a475aa7de7cef48b136126f
moy-boy <173254869+moy-boy@users.noreply.github.com>
2024-08-09 16:55:40 -0700

    Adding a centralized logger

--- agentifyme/__init__.py
+++ agentifyme/__init__.py
@@ -1,11 +1,6 @@
 from agentifyme.config import AgentifyMeConfig
+from agentifyme.logger import get_logger
 from agentifyme.tasks import task
-from agentifyme.utilities.log import getLogger
 from agentifyme.workflows import workflow
 
-
-def version():
-    return "0.1.0"
-
-
-__all__ = ["getLogger", "AgentifyMeConfig", "task", "workflow", "version"]
+__all__ = ["get_logger", "AgentifyMeConfig", "task", "workflow"]
