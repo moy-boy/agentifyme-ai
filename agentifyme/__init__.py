d1bf4bb2cd3f9beef9de1a8a60d4903491503ef9
moy-boy <173254869+moy-boy@users.noreply.github.com>
2025-02-04 22:14:23 -0800

    Bump package version to 0.1.53

--- agentifyme/__init__.py
+++ agentifyme/__init__.py
@@ -12,7 +12,7 @@ from agentifyme.errors import (
 )
 from agentifyme.logger import get_logger
 
-__version__ = "0.1.52"
+__version__ = "0.1.53"
 __all__ = [
     "AgentifyMeError",
     "AgentifyMeExecutionError",
