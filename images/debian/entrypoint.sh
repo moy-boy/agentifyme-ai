3fc008e8415de227a8cae736cf063afb631c7094
moy-boy <173254869+moy-boy@users.noreply.github.com>
2025-03-20 12:40:50 -0700

    Refine PYTHON_LIB_PATH extraction in entrypoint script to improve reliability

--- images/debian/entrypoint.sh
+++ images/debian/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-PYTHON_LIB_PATH=$(find ${HOME}/.local/share/uv/python -name "libpython*.so*" -exec dirname {} \; | head -n 1)
+PYTHON_LIB_PATH=$(find ${HOME}/.local/share/uv/python -name "libpython*.so*" | sed 's:/[^/]*$::' | head -n 1)
 if [ -n "$PYTHON_LIB_PATH" ]; then
   export LD_LIBRARY_PATH="$PYTHON_LIB_PATH:${LD_LIBRARY_PATH:-}"
 else
