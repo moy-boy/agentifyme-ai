fc77bb6826f69744bf1ca027df265310cba9f722
moy-boy <173254869+moy-boy@users.noreply.github.com>
2025-06-02 14:15:08 -0700

    Update __init__.py

--- agentifyme/__init__.py
+++ agentifyme/__init__.py
@@ -12,7 +12,7 @@ from agentifyme.errors import (
 )
 from agentifyme.logger import get_logger
 
-__version__ = "0.2.13"
+__version__ = "0.3.0"
 __all__ = [
     "AgentifyMeError",
     "AgentifyMeExecutionError",
